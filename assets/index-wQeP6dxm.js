cd890583efa7e51e15b3e1f93b490d8a0dbcb27d
olivaresmuller-png <olivaresmuller@gmail.com>
2026-01-11 13:06:37 +0100

    Delete assets directory

--- assets/index-wQeP6dxm.js
+++
@@ -1,6 +0,0 @@
-import{c as n,r as t}from"./index-YWz30tJV.js";/**
- * @license lucide-react v0.545.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const o=[["path",{d:"M20 6 9 17l-5-5",key:"1gmf2c"}]],i=n("check",o);function a(e,[r,c]){return Math.min(c,Math.max(r,e))}var u=t.createContext(void 0);function f(e){const r=t.useContext(u);return e||r||"ltr"}function p(e){const r=t.useRef({value:e,previous:e});return t.useMemo(()=>(r.current.value!==e&&(r.current.previous=r.current.value,r.current.value=e),r.current.previous),[e])}export{i as C,p as a,a as c,f as u};
